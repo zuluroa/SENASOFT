da9e162fa70396c55a872b8ef545b05926cc02b8
galvis1999 <juangalvis@wposs.com>
2021-10-20 22:40:24 -0500

    modificacion de conexion

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect('localhost', 'root', '', 'vitalplus') or die("No se pudo establecer conexion con la base de datos");
+$conexion = mysqli_connect('localhost', 'root', '', 'sena_soft') or die("No se pudo establecer conexion con la base de datos");
 setlocale(LC_ALL, 'es_CO', 'Spanish_Colombia', 'Spanish');
 date_default_timezone_set('America/Bogota');
 ?>
